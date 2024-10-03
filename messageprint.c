868ceb90a619fb90b2b1782b9384714216c396ef
SteveCharles50 <112490403+SteveCharles50@users.noreply.github.com>
2024-10-03 11:17:25 -0400

    Update messageprint.c

--- messageprint.c
+++ messageprint.c
@@ -24,6 +24,9 @@ int prime = 0;
 int isPrime(int num){
     //assumes number is not prime
     int checkPrime = 0;
+    if(num <= 1){
+        return 0;
+    }
     for(int b = 2; b < num; b++){
         if(num % b == 0){
             checkPrime = 0;
@@ -39,36 +42,36 @@ int isPrime(int num){
 
 //routine for thread creation
 void* routine(void* arg){
-    //printing threadID for current calling thread
-    printf("Here is the thread ID: %lu \n", (long unsigned) pthread_self());
-
+    pthread_mutex_lock(&lock);
+    printf("This is a new thread processing\n");
+    printf("Here is the tracker val: %d\n", tracker);
     //calls function to check if thread is prime or composite
     unsigned long tid = (unsigned long)pthread_self();
     prime = isPrime((int)tid);
-    printf("%d\n", prime);
+    printf("prime val: %d\n", prime);
+    if(prime == 1){
+        printf("Here is the prime: %d\n", prime);
 
+    }
     //getting threadID will work off of this to process composite and prime threadID
     if(prime == 0 && tracker == numOfThreads){
-        pthread_mutex_lock(&lock);
-        pthread_mutex_unlock(&lock);
         tracker++;
+        pthread_mutex_unlock(&lock);
         pthread_cond_broadcast(&CV);
     }
     else if(prime == 0){
-        pthread_mutex_lock(&lock);
-        //printf("Here is the thread ID: %lu \n", (long unsigned) pthread_self());
-        pthread_mutex_unlock(&lock);
+        printf("Here is the thread ID: %lu \n", (long unsigned) pthread_self());
         tracker++;
+        pthread_mutex_unlock(&lock);
     }
     else{
-        tracker++;
-        pthread_mutex_lock(&lock);
         while(number_prime_finished == 0){
             printf("ThreadID in the else block: %lu\n", (long unsigned) pthread_self());
+            tracker++;
             printf("Executing Prime threads first...\n");
             pthread_cond_wait(&CV, &lock);
+            pthread_mutex_unlock(&lock);
         }
-        pthread_mutex_unlock(&lock);
     }
     return 0;
 }
@@ -104,3 +107,5 @@ int main(int argc, char* argv[]){
     printf("tracker: %d\n", tracker);
     return 0;
 }
+
+
